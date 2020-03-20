0a5630aec696d11becc9a48f1df2d9891a4f0dd2
Denis Kapaev <d.kapaev@yandex.ru>
2020-03-20 18:11:06 +0300

    Customize toolbar

--- app/src/main/java/ru/skillbranch/skillarticles/ui/RootActivity.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/RootActivity.kt
@@ -2,12 +2,33 @@ package ru.skillbranch.skillarticles.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.widget.Toolbar
+import kotlinx.android.synthetic.main.activity_root.*
 import ru.skillbranch.skillarticles.R
+import ru.skillbranch.skillarticles.extensions.dpToIntPx
 
 class RootActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_root)
+        setupToolbar()
+    }
+
+    private fun setupToolbar() {
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        if (toolbar.childCount > 2 && toolbar.getChildAt(2) is ImageView) {
+            val toolbarLogo = toolbar.getChildAt(2) as ImageView
+            toolbarLogo.scaleType = ImageView.ScaleType.CENTER_CROP
+            val layoutParams = toolbarLogo.layoutParams as? Toolbar.LayoutParams
+            layoutParams?.apply {
+                width = dpToIntPx(40)
+                height = dpToIntPx(40)
+                marginEnd = dpToIntPx(16)
+                toolbarLogo.layoutParams = this
+            }
+        }
     }
 }

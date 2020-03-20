a307d7adf9092ba41454596c6badaa8b9cbdfa74
Denis Kapaev <d.kapaev@yandex.ru>
2020-03-20 20:13:57 +0300

    Add IArticleViewModel contract

--- app/src/main/java/ru/skillbranch/skillarticles/viewmodels/ArticleViewModel.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/viewmodels/ArticleViewModel.kt
@@ -8,7 +8,7 @@ import ru.skillbranch.skillarticles.extensions.data.toAppSettings
 import ru.skillbranch.skillarticles.extensions.data.toArticlePersonalInfo
 import ru.skillbranch.skillarticles.extensions.format
 
-class ArticleViewModel(private val articleId: String) : BaseViewModel<ArticleState>(ArticleState()) {
+class ArticleViewModel(private val articleId: String) : BaseViewModel<ArticleState>(ArticleState()), IArticleViewModel {
 
     private val repository = ArticleRepository
 
@@ -50,37 +50,37 @@ class ArticleViewModel(private val articleId: String) : BaseViewModel<ArticleSta
     }
 
     // load text from network
-    private fun getArticleContent(): LiveData<List<Any>?> {
+    override fun getArticleContent(): LiveData<List<Any>?> {
         return repository.loadArticleContent(articleId)
     }
 
     // load data from db
-    private fun getArticleData(): LiveData<ArticleData?> {
+    override fun getArticleData(): LiveData<ArticleData?> {
         return repository.getArticle(articleId)
     }
 
     // load data from db
-    private fun getArticlePersonalInfo(): LiveData<ArticlePersonalInfo?> {
+    override fun getArticlePersonalInfo(): LiveData<ArticlePersonalInfo?> {
         return repository.loadArticlePersonalInfo(articleId)
     }
 
     // app settings
-    fun handleUpText() {
+    override fun handleUpText() {
         val settings = currentState.toAppSettings()
         repository.updateSettings(settings.copy(isBigText = true))
     }
 
-    fun handleDownText() {
+    override fun handleDownText() {
         val settings = currentState.toAppSettings()
         repository.updateSettings(settings.copy(isBigText = false))
     }
 
-    fun handleNightMode() {
+    override fun handleNightMode() {
         val settings = currentState.toAppSettings()
         repository.updateSettings(settings.copy(isDarkMode = !settings.isDarkMode))
     }
 
-    fun handleLike() {
+    override fun handleLike() {
         val toggleLike = {
             val info = currentState.toArticlePersonalInfo()
             repository.updateArticlePersonalInfo(info.copy(isLike = !info.isLike))
@@ -101,17 +101,25 @@ class ArticleViewModel(private val articleId: String) : BaseViewModel<ArticleSta
         notify(msg)
     }
 
-    fun handleBookmark() {
+    override fun handleBookmark() {
         // TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
     }
 
-    fun handleShare() {
+    override fun handleShare() {
         val msg = "Share is not implemented"
         notify(Notify.ErrorMessage(msg, "OK", null))
     }
 
-    fun handleToggleMenu() {
+    override fun handleToggleMenu() {
         updateState { it.copy(isShowMenu = !it.isShowMenu) }
+    }
+
+    override fun handleSearchMode(isSearch: Boolean) {
+        // TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun handleSearch(query: String?) {
+        // TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
     }
 }
 
---
+++ app/src/main/java/ru/skillbranch/skillarticles/viewmodels/IArticleViewModel.kt
@@ -0,0 +1,82 @@
+package ru.skillbranch.skillarticles.viewmodels
+
+import androidx.lifecycle.LiveData
+import ru.skillbranch.skillarticles.data.ArticleData
+import ru.skillbranch.skillarticles.data.ArticlePersonalInfo
+
+interface IArticleViewModel {
+    /**
+     * Получение полной информации о статье из сети
+     * (или базы данных если она сохранена, наличие статьи в базе не надо реализовывать в данном уроке)
+     */
+    fun getArticleContent(): LiveData<List<Any>?>
+
+    /**
+     * Получение краткой информации о статье из базы данных
+     */
+    fun getArticleData(): LiveData<ArticleData?>
+
+    /**
+     * Получение пользовательской информации о статье из базы данных
+     */
+    fun getArticlePersonalInfo(): LiveData<ArticlePersonalInfo?>
+
+    /**
+     * Получение настроек приложения
+     */
+    fun handleNightMode()
+
+    /**
+     * Обработка нажатия на btn_text_up (увеличение шрифта текста)
+     * необходимо увеличить шрифт до значения 18
+     */
+    fun handleUpText()
+
+    /**
+     * Обработка нажатия на btn_text_down (стандартный размер шрифта)
+     * необходимо установить размер шрифта по умолчанию 14
+     */
+    fun handleDownText()
+
+    /**
+     * добавление/удаление статьи в закладки, обработка нажатия на кнопку btn_bookmark
+     * необходимо отобразить сообщение пользователю "Add to bookmarks" или "Remove from bookmarks"
+     * в соответствии с текущим состоянием
+     */
+    fun handleBookmark()
+
+    /**
+     * добавление/удаление статьи в понравившееся, обработка нажатия на кнопку btn_like
+     * необходимо отобразить сообщение пользователю (Notify.ActionMessage) "Mark is liked" или
+     * "Don`t like it anymore"  в соответствии с текущим состоянием.
+     * если пользователь убрал Like необходимо добавить  actionLabel в снекбар
+     * "No, still like it" при нажатии на который состояние вернется к isLike = true
+     */
+    fun handleLike()
+
+    /**
+     * поделиться статьей, обработка нажатия на кнопку btn_share
+     * необходимо отобразить сообщение с ошибкой пользователю (Notify.ErrorMessage) "Share is not implemented"
+     * и текстом errLabel "OK"
+     */
+    fun handleShare()
+
+    /**
+     * обработка нажатия на кнопку btn_settings
+     * необходимо отобразить или скрыть меню в соответствии с текущим состоянием
+     */
+    fun handleToggleMenu()
+
+    /**
+     * обработка перехода в режим поиска searchView
+     * при нажатии на пункту меню тулбара необходимо отобразить searchView и сохранить состояние при
+     * изменении конфигурации (пересоздании активити)
+     */
+    fun handleSearchMode(isSearch: Boolean)
+
+    /**
+     * обработка поискового запроса, необходимо сохранить поисковый запрос и отображать его в
+     * searchView при изменении конфигурации (пересоздании активити)
+     */
+    fun handleSearch(query: String?)
+}

c2b4dc0d8e85e31479726c309bc5e1f33dd2fa8c
Denis Kapaev <d.kapaev@yandex.ru>
2020-03-20 18:15:33 +0300

    Add BottomBar

---
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/custom/Bottombar.kt
@@ -0,0 +1,18 @@
+package ru.skillbranch.skillarticles.ui.custom
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.FrameLayout
+import ru.skillbranch.skillarticles.R
+
+class Bottombar @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr) {
+    init {
+        val view = View.inflate(context, R.layout.layout_bottombar, null)
+        addView(view)
+    }
+}

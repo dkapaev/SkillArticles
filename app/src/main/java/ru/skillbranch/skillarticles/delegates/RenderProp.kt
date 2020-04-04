9f103950c8684976d5db59fa4c5e155d79a4d9a1
Denis Kapaev <d.kapaev@yandex.ru>
2020-04-04 14:05:47 +0300

    Add ObserveProp delegate

--- app/src/main/java/ru/skillbranch/skillarticles/delegates/RenderProp.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/delegates/RenderProp.kt
@@ -9,6 +9,7 @@ class RenderProp<T>(
     needInit: Boolean = true,
     private val onChange: ((T) -> Unit)? = null
 ) : ReadWriteProperty<Binding, T> {
+    private val listeners: MutableList<() -> Unit> = mutableListOf()
 
     init {
         if (needInit) onChange?.invoke(value)
@@ -22,6 +23,35 @@ class RenderProp<T>(
         if (this.value != value) {
             this.value = value
             onChange?.invoke(this.value)
+            if (listeners.isNotEmpty()) listeners.forEach { it.invoke() }
         }
+    }
+
+    // register additional listener
+    fun addListener(listener: () -> Unit) {
+    }
+}
+
+class ObserveProp<T : Any> (
+    private var value: T,
+    private val onChange: ((T) -> Unit)? = null
+) {
+    // provide delegate (when "by" is called)
+    operator fun provideDelegate(
+        thisRef: Binding,
+        prop: KProperty<*>
+    ): ReadWriteProperty<Binding, T> {
+        val delegate = RenderProp(value, true, onChange)
+        registerDelegate(thisRef, prop.name, delegate)
+        return delegate
+    }
+
+    // register new delegate for property in Binding
+    private fun registerDelegate(
+        thisRef: Binding,
+        name: String,
+        delegate: RenderProp<T>
+    ) {
+        thisRef.delegates[name] = delegate
     }
 }
--- app/src/main/java/ru/skillbranch/skillarticles/ui/base/Binding.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/base/Binding.kt
@@ -1,9 +1,12 @@
 package ru.skillbranch.skillarticles.ui.base
 
 import android.os.Bundle
+import ru.skillbranch.skillarticles.delegates.RenderProp
 import ru.skillbranch.skillarticles.viewmodels.base.IViewModelState
 
 abstract class Binding {
+    val delegates = mutableMapOf<String, RenderProp<out Any>>()
+
     abstract fun onFinishInflate()
     abstract fun bind(data: IViewModelState)
     fun saveUi(outState: Bundle) {

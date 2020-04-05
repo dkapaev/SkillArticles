dbc4f2b973231d43abf96a6049648b5814f33b53
Denis Kapaev <d.kapaev@yandex.ru>
2020-04-05 16:14:38 +0300

    Fix indexesOf - add nullable receiver

--- app/src/main/java/ru/skillbranch/skillarticles/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/extensions/String.kt
@@ -1,8 +1,8 @@
 package ru.skillbranch.skillarticles.extensions
 
-fun String.indexesOf(subStr: String, ignoreCase: Boolean = true): List<Int> {
+fun String?.indexesOf(subStr: String, ignoreCase: Boolean = true): List<Int> {
     val result = ArrayList<Int>()
-    if (this.isNotEmpty() && subStr.isNotEmpty()) {
+    if (this != null && this.isNotEmpty() && subStr.isNotEmpty()) {
         var lastIndex = 0
         while (lastIndex != -1) {
             lastIndex = this.indexOf(subStr, lastIndex, ignoreCase)
